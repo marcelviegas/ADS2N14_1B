4c4c623b076d25d247b0803d656db01cf0963ca8
marcelviegas <psc_marcelviegas@hotmail.com>
2014-02-23 19:23:54 -0300

    adicionando a classe pessoa
    classe adicionada

---
+++ pessoa.java
@@ -0,0 +1,27 @@
+//classe pessoa
+public class pessoa {
+	
+	//Meu atributo
+	String username;
+	
+	//metodo contrutor 
+	public pessoa(){
+		
+		
+		}
+	//metodo para buscar o nome da pessoa
+	public String getUsername() {
+		return username;
+	}
+    //metodo para setar o nome da pessoa
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	 
+	
+	
+	
+	
+	
+}
