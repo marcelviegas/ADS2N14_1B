e56d1e268aa25ebadae4401586121a8014be77c1
marcelviegas <psc_marcelviegas@hotmail.com>
2014-02-23 18:50:11 -0300

    Revert "atualizacao do trabalho"
    This reverts commit 1385837929a5b19b7bf04bad2d9cae5fac4caaa1.

--- helloworld.java
+++ helloworld.java
@@ -1,20 +1,2 @@
-public class helloworld {
-
-	/**
-	 * @param args
-	 */
-	
-	
-	public static void main(String[] args) {
-		//Instanciando a classe
-		pessoa user = new pessoa();
-		//Setando o atributo
-		user.setUsername("marcel");
-		// TODO Auto-generated method stub
-		//Imprimindo em tela concatenado com o atributo setado anteriormente
-		System.out.println("hello "+user.getUsername());
-
-	}
-
-}
+system.out.print("hello world")
 
