6d05b2b603db22437c2b4a6c1320ad5455653dd5
Marcel Viegas <psc_marcelviegas@hotmail.com>
2014-02-25 22:33:53 -0300

    teste

--- helloworld.java
+++ helloworld.java
@@ -1,2 +1,20 @@
-system.out.print("hello world")
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		//Instanciando a classe
+		pessoa user = new pessoa();
+		//Setando o atributo
+		user.setUsername("marcel");
+		// TODO Auto-generated method stub
+		//Imprimindo em tela concatenado com o atributo setado anteriormente
+		System.out.println("hello "+user.getUsername());
+
+	}
+
+}
 
