4bd064687f960d755ea197ffef61436319735991
Marcel Viegas <psc_marcelviegas@hotmail.com>
2014-03-15 15:14:00 -0300

    insercao da classe telefone e pessoa

---
+++ Agenda/src/com/senac/agenda/Pessoa.java
@@ -0,0 +1,22 @@
+package com.senac.agenda;
+
+public class Pessoa {
+	
+	
+	private String nome;
+	private String endereco;
+    private Telefone fone;
+    
+    
+	public Pessoa(String nome, String endereco, Telefone fone) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.fone = fone;
+	}
+    
+	
+	
+	
+    
+}
---
+++ Agenda/src/com/senac/agenda/Telefone.java
@@ -0,0 +1,39 @@
+package com.senac.agenda;
+
+public class Telefone {
+	
+ private int ddd;
+ private double numero;
+ private int tipoTelefone;
+ 
+ 
+public Telefone(int ddd, double numero, int tipoTelefone) {
+	super();
+	this.ddd = ddd;
+	this.numero = numero;
+	this.tipoTelefone = tipoTelefone;
+}
+public int getDdd() {
+	return ddd;
+}
+public void setDdd(int ddd) {
+	this.ddd = ddd;
+}
+public double getNumero() {
+	return numero;
+}
+public void setNumero(double numero) {
+	this.numero = numero;
+}
+public int getTipoTelefone() {
+	return tipoTelefone;
+}
+public void setTipoTelefone(int tipoTelefone) {
+	this.tipoTelefone = tipoTelefone;
+}
+ 
+ 
+ 
+ 
+
+}
